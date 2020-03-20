1708f83217643319719ad29417b489475ac4afab
Craig Contreras <46203062+CraigCaseyContreras@users.noreply.github.com>
2020-03-20 10:36:35 -0400

    Update Traveling salesman.py

--- TRAVELING_SALESMAN/Traveling salesman.py
+++ TRAVELING_SALESMAN/Traveling salesman.py
@@ -10,9 +10,49 @@ class initialize:
     
     def __init__(self, cities, city_names, populationSize):
         self.cities = cities
-        self.city_name = city_names
+        self.city_names = city_names
         self.populationSize = populationSize
         #self.cityList = cityList
+    
+
+
+    def generatePath(self):
+        path = random.sample(self.cities, len(self.cities))
+        return path
+
+    def plot_pop(self, pop_plot):
+        xList = []
+        yList = []
+        for i in range(len(self.cities)):
+            xList.append(self.cities[i][0])
+            yList.append(self.cities[i][1])
+
+        plt.figure(figsize=(20,10))
+        x = [i[0] for i in pop_plot]
+        y = [i[1] for i in pop_plot]
+        x1=[x[0],x[-1]]
+        y1=[y[0],y[-1]]
+        plt.plot(x, y, 'b', x1, y1, 'b')
+        plt.scatter (x, y) 
+
+        for i, txt in enumerate(self.city_names):
+            plt.annotate(txt, (xList[i], yList[i]),horizontalalignment='center', 
+                verticalalignment='bottom',
+                        )
+            print(txt, xList[i], yList[i])
+        plt.show()
+        return
+
+    def initialPopulation(self):
+        population = [self.generatePath() for i in range(0, self.populationSize)]
+        return population
+
+
+class GeneticAlgorithm:
+    
+    #def __init__(self, cities):
+        #self.cities = cities
+        
     
     def distance_between_cities(self, cities):
         data = dict()
@@ -36,70 +76,50 @@ class initialize:
         return data
 
     def total_distance(self, cities):
-        total = sum(distance_between_cities(cities).values())
+        total = sum(self.distance_between_cities(cities).values())
         return total
-
-    def generatePath(self):
-        path = random.sample(self.cities, len(self.cities))
-        return path
-
-    def plot_pop(self, cities, city_names, cityList):
-        xList = []
-        yList = []
-        for i in range(len(cityList)):
-            xList.append(cityList[i][0])
-            yList.append(cityList[i][1])
-
-        plt.figure(figsize=(20,10))
-        x = [i[0] for i in cities]
-        y = [i[1] for i in cities]
-        x1=[x[0],x[-1]]
-        y1=[y[0],y[-1]]
-        plt.plot(x, y, 'b', x1, y1, 'b')
-        plt.scatter (x, y) 
-
-        for i, txt in enumerate(city_names):
-            plt.annotate(txt, (xList[i], yList[i]),horizontalalignment='center', 
-                verticalalignment='bottom',
-                        )
-            print(txt, xList[i], yList[i])
-        plt.show()
-        return
-
-    def initialPopulation(self):
-        population = [self.generatePath() for i in range(0, self.populationSize)]
-        return population
-
-
-class GeneticAlgorithm:
-    def __init(self):
-        return
     
     #For the fitness levels, I base it off of total distance. We can decide on this, but I get the total distances and 
     #do 1/totalDistance.
-    def path_fitness(self, cities):
-        total_dis = self.total_distance(cities)
+    def path_fitness(self, people):
+        total_dis = self.total_distance(people)
         fitness = 1 / float(total_dis)
         return fitness
+    
+    #Rank them bitches
+    def rankPathes(self, population):
+        results = {}
+        for i in range(len(population)):
+            results[i] = self.path_fitness(population[i])
+            
+        #This just sorts them in order from greated fitness to least fitness
+        # population #: fitness value. There are 10 in total because made 10 populations
+        return sorted(results.items(), key = operator.itemgetter(1), reverse = True)
 
 if __name__ == '__main__':
     f = open("TSM.txt", 'r').read().splitlines()
+    #List of cities essentially
     cityCoords = np.array([ tuple( map( float, coord.split() ) ) for coord in f ]).tolist()
-    #print("City Coords: ", cityCoords)
-    #val = distance_between_cities(cityCoords).values()
-    
-    #print(list)
     city_names = open("city_names.txt", 'r').read().splitlines()
+    popSize = 10
     
-    initial = initialize(cityCoords, city_names, 10)
-    #list= initial.generatePath()
+    #Call the class object
+    initial = initialize(cityCoords, city_names, popSize)
+    
+    listOfPaths= initial.generatePath()
+    #distances = initial.distance_between_cities(cityCoords).values()
     
     population = initial.initialPopulation()
+    
     for idx, pop_plot in enumerate(population):
         print('Init pop. ' + str(idx), pop_plot)
         print('\n')
-        initial.plot_pop(pop_plot, city_names, cityCoords)
+        initial.plot_pop(pop_plot)
 
+    genetics = GeneticAlgorithm()
+    fitness = genetics.path_fitness(cityCoords)
+    ranks = genetics.rankPathes(population)
+    
 #I still gotta fix the functions to make sure they all work fine in the class, but it works.
 #Now we just need to do the Genetic Algorithm stuff. We can do that in the class.
 #After the fitness, we need to rank them, select, and mate/breed the bitches.
